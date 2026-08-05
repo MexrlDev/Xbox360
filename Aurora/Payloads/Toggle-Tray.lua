0f2607ade8da07ec127c0b90c98311abf4f75a87
MexrlDev <82407748+MexrlDev@users.noreply.github.com>
2026-08-06 01:53:08 +0300

    Update Toggle-Tray.lua

--- Aurora/Payloads/Toggle-Tray.lua
+++ Aurora/Payloads/Toggle-Tray.lua
@@ -1,9 +1,9 @@
-local state = 0
-pcall(function() state = Aurora.GetDVDTrayState() end)
-if state == 0 then
-    pcall(function() Aurora.OpenDVDTray() end)
-    Script.ShowNotification("Opening DVD tray")
-else
-    pcall(function() Aurora.CloseDVDTray() end)
-    Script.ShowNotification("Closing DVD tray")
-end
+pcall(function()
+    if Aurora.GetDVDTrayState() == 0 then
+        Aurora.OpenDVDTray()
+        Script.ShowNotification("Opening DVD tray")
+    else
+        Aurora.CloseDVDTray()
+        Script.ShowNotification("Closing DVD tray")
+    end
+end)
