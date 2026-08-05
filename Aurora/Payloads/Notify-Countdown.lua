03575ee9a24cd59d2a66c49ec25ea0e09bfe4841
MexrlDev <82407748+MexrlDev@users.noreply.github.com>
2026-08-06 00:46:49 +0300

    Update Notify-Countdown.lua

--- Aurora/Payloads/Notify-Countdown.lua
+++ Aurora/Payloads/Notify-Countdown.lua
@@ -1,7 +1,7 @@
--function main()
+function main()
     for i = 5, 1, -1 do
         Script.ShowNotification("Countdown: " .. i)
-        -- wait 1 second using a simple busy loop
+        -- wait 1 second (simple busy loop)
         local target = os.time() + 1
         while os.time() < target do
         end
