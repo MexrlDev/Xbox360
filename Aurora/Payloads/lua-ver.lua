86d87a1624cb1eec0220ca46185d4737a8701ca9
MexrlDev <82407748+MexrlDev@users.noreply.github.com>
2026-08-06 00:05:32 +0300

    Update lua-ver.lua

--- Aurora/Payloads/lua-ver.lua
+++ Aurora/Payloads/lua-ver.lua
@@ -1,4 +1,1 @@
-function main()
-    local version = _VERSION or "Lua 5.1 (Aurora)"
-    Script.ShowNotification("Lua version: " .. tostring(version))
-end
+Script.ShowNotification("Lua version: " .. (_VERSION or "Lua 5.1 (Aurora)"))
